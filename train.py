7a35da25792c2526d96fe2956ecdb061146e7c7e
yangyi <1260782789@qq.com>
2018-11-26 14:03:32 +0800

    git tracks changes

--- train.py
+++ train.py
@@ -5,3 +5,6 @@ Git is a distributed version control system.
 Git is free software distributed under the GPL.
 
 Git has a mutable index called stage.
+
+Git tracks changes of files.
+I modified this file at the 1st time.
